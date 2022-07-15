b13ca9b8361aaf39c4e5c4a43ab772107eb7719d
Corentin Bourdat <cbourdat@hexagonalconsulting.com>
2022-07-15 13:56:27 +0200

    fix reservation controller

---
+++ src/main/java/com/testoc/apitest/controller/ReservationController.java
@@ -0,0 +1,32 @@
+kage com.testoc.apitest.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testoc.apitest.model.Reservation;
+import com.testoc.apitest.service.ReservationService;
+
+@RestController
+public class ReservationController {
+
+  @Autowired
+  private ReservationService reservationService;
+
+  /**
+   * Create - Add a new employee
+   * @param reservation An object employee
+   * @return The employee object saved
+   */
+  @PostMapping("/reservation")
+  public Reservation createReservation(@RequestBody Reservation reservation) {
+    return reservationService.saveReservation(reservation);
+  }
+}
